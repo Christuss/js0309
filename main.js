c539b6429d9874570d896d5c868dfeb5c8150593
Christuss <szendefi.krisztofer@gmail.com>
2023-03-10 11:03:19 +0100

    kepgaleria

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", init);
 
+
 function init() {
+    var kepHely;
     const KEPEK = ["https://th.bing.com/th/id/R.16d0248bd00fcf7d3bddccee0a00cee7?rik=cLkXOSX6RR7OKQ&riu=http%3a%2f%2fstatic.keptelenseg.hu%2fp%2f659074f25f1d646c6f2ea9015a9cb3b9.jpg&ehk=ynB6NIWJBDT%2b1KmF8Iybbz1iphJm02qi1FQ%2fwi6qCrw%3d&risl=&pid=ImgRaw&r=0",
         "https://static.keptelenseg.hu/p/7b4aabd5caac3540a8c9f57472d21c22.jpg",
         "https://th.bing.com/th/id/R.ad450b211608940b45e226af71e30356?rik=5wR2HY1LHJ0o1Q&riu=http%3a%2f%2fstatic.keptelenseg.hu%2fp%2fc94ca6784fc64b1c5a76d6ab718580f4.jpg&ehk=ELtu%2fd65lH23DkAIuxqK0hqUE0lXwja9YullTvYEqTw%3d&risl=&pid=ImgRaw&r=0",
@@ -13,10 +15,52 @@ function init() {
     const ARTICLE = document.querySelectorAll("article");
     for (let index = 0; index < KEPEK.length; index++) {
         ARTICLE[0].innerHTML += '<div><img src="' + KEPEK[index] + '" alt="kep"></div>';
-        const KEP = document.querySelectorAll("img");
+    }
+
+    const NAGYKEP = document.querySelectorAll('section div');
+    NAGYKEP[0].innerHTML = '<img src="' + KEPEK[0] + '" alt="kep">';
+    const KEP = document.querySelectorAll("article img");
+    for (let index = 0; index < KEPEK.length; index++) {
         KEP[index].addEventListener('click', function () {
             console.log(KEPEK[index]);
+            const NAGYKEP = document.querySelectorAll('section div');
+            NAGYKEP[0].innerHTML = '<img src="' + KEPEK[index] + '" alt="kep">';
         });
     }
 
+    const GOMB = document.querySelectorAll("section button");
+    GOMB[0].addEventListener('click', function () {
+        const NAGYKEP = document.querySelectorAll('section div');
+        const KEP = document.querySelectorAll('section div img');
+        var kepNev = KEP[0].getAttribute('src');
+        for (let index = 0; index < KEPEK.length; index++) {
+            if (KEPEK[index] == kepNev) {
+                kepHely = index;
+            }
+        }
+        if (kepHely == 0) {
+            kepHely = KEPEK.length - 1;
+            NAGYKEP[0].innerHTML = '<img src="' + KEPEK[kepHely] + '" alt="kep">';
+        } else {
+            NAGYKEP[0].innerHTML = '<img src="' + KEPEK[kepHely - 1] + '" alt="kep">';
+        }
+    });
+
+    GOMB[1].addEventListener('click', function () {
+        const NAGYKEP = document.querySelectorAll('section div');
+        const KEP = document.querySelectorAll('section div img');
+        var kepNev = KEP[0].getAttribute('src');
+        for (let index = 0; index < KEPEK.length; index++) {
+            if (KEPEK[index] == kepNev) {
+                kepHely = index;
+            }
+        }
+        if (kepHely == KEPEK.length - 1) {
+            kepHely = 0;
+            NAGYKEP[0].innerHTML = '<img src="' + KEPEK[kepHely] + '" alt="kep">';
+        } else {
+            NAGYKEP[0].innerHTML = '<img src="' + KEPEK[kepHely + 1] + '" alt="kep">';
+        }
+
+    });
 }
